94437e2523faf0734741bf7fa2faf5f09f6cc258
sabide <32515178+sabide@users.noreply.github.com>
2017-10-12 09:15:20 +0200

    Update tu01-lu.f90

--- LU-serial/tu01-lu.f90
+++ LU-serial/tu01-lu.f90
@@ -1,12 +1,14 @@
 program main
   use module_lu_chps
   implicit none
-  real(kind=8),dimension(:,:),allocatable :: A
+  real(kind=8),dimension(:,:),allocatable :: A,L,U
   real(kind=8),dimension(:),allocatable :: b,x,y
   integer :: i,j,p
   integer :: nx,ny
 
   allocate(A(1:4,1:4))
+  allocate(L(1:4,1:4))
+  allocate(U(1:4,1:4))
   allocate(b(1:4),x(1:4),y(1:4))
   
   A(1,1:4) = (/2,1,1,0/)
@@ -22,6 +24,20 @@ program main
   end do
   
   call LU_FACTORIZE(A)
+  
+  L=0
+  L(1,1)=1
+  DO I=2,4
+     L(I,I)=1
+     L(I,1:I-1)=A(I,1:I-1)
+  END DO
+  U=0
+  DO I=1,4
+     U(I,I:4)=A(I,I:4)
+  END DO
+  A=matmul(L,U)
+  
+  
   call L_SOLVE(A,Y,B)
   call U_SOLVE(A,X,Y)
   
